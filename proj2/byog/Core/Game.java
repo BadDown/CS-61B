efcee90645136f69ce09a1d35e661669e2fcf714
BadDown <Mr_Lsy@outlook.com>
2022-06-14 06:29:00 -0700

    finish proj2

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -1,7 +1,15 @@
 package byog.Core;
 
+import byog.SaveDemo.World;
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
+import edu.princeton.cs.introcs.StdDraw;
+
+import java.awt.*;
+import java.io.*;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Game {
     TERenderer ter = new TERenderer();
@@ -9,10 +17,38 @@ public class Game {
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
 
+    public static final int canvasWIDTH = 768;
+    public static final int canvasHEIGHT = 1024;
+
+    public static boolean gameOver = false;
+
+
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
      */
     public void playWithKeyboard() {
+        char firstChar;
+        drawMenu();
+        firstChar = getFirstChar();
+        TETile[][] world;
+        switch (firstChar) {
+            case 'n':
+                Long Seed = getSeed();
+                world = generateWorld(Seed);
+                generateInitWorld(world);
+                startGame(world, null);
+                break;
+            case 'l':
+                world = loadWorld();
+                generateInitWorld(world);
+                startGame(world, null);
+                break;
+            case 'q':
+                System.exit(0);
+                break;
+            default:
+                break;
+        }
     }
 
     /**
@@ -33,6 +69,322 @@ public class Game {
         // drawn if the same inputs had been given to playWithKeyboard().
 
         TETile[][] finalWorldFrame = null;
+
+        input = input.toLowerCase();
+        char firstChar = input.charAt(0);
+        switch (firstChar) {
+            case 'n':
+                long Seed = getInputSeed(input);
+                input = input.substring(input.indexOf('s' + 1, input.length()));
+                finalWorldFrame = generateWorld(Seed);
+                generateInitWorld(finalWorldFrame);
+                startGame(finalWorldFrame, input);
+                break;
+            case 'l':
+                finalWorldFrame = loadWorld();
+                generateInitWorld(finalWorldFrame);
+                break;
+            case 'q':
+                System.exit(0);
+                break;
+            default:
+                System.out.println("input is error !");
+                break;
+        }
+
         return finalWorldFrame;
+    }
+
+    private void drawMenu() {
+        InitializeCanvas(canvasWIDTH, canvasHEIGHT);
+
+        Font bigFont = new Font("Verdana", Font.BOLD, 22);
+        StdDraw.setFont(bigFont);
+        StdDraw.text(canvasWIDTH / 2, canvasHEIGHT * 2 / 3, "CS61B: THE GAME");
+        Font smallFont = new Font("Verdana", Font.PLAIN, 16);
+        StdDraw.text(canvasWIDTH / 2, canvasHEIGHT / 2, "Load Game (L)");
+        StdDraw.text(canvasWIDTH / 2, canvasHEIGHT / 2 + 40, "New Game (N)");
+        StdDraw.text(canvasWIDTH / 2, canvasHEIGHT / 2 - 40, "Quit (Q)");
+
+        StdDraw.show();
+    }
+
+    private void InitializeCanvas(int width, int height) {
+
+        StdDraw.setCanvasSize(width, height);
+        StdDraw.setXscale(0, width);
+        StdDraw.setYscale(0, height);
+        StdDraw.clear(Color.BLACK);
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setPenColor(Color.white);
+    }
+
+    private char getFirstChar() {
+        char firstChar;
+        while (true) {
+            if (!StdDraw.hasNextKeyTyped()) {
+                continue;
+            }
+            firstChar = Character.toLowerCase(StdDraw.nextKeyTyped());
+            if (firstChar == 'n' || firstChar == 'l' || firstChar == 'q') {
+                return firstChar;
+            }
+        }
+    }
+
+    private Long getSeed() {
+        StdDraw.clear(Color.BLACK);
+        Font font = new Font("Verdana", Font.BOLD, 16);
+        StdDraw.setFont(font);
+        StdDraw.text(canvasWIDTH / 2, canvasHEIGHT / 2, "Please input the seed and end with 'S'!");
+        StdDraw.show();
+
+        String Seed = "";
+        while (true) {
+            if (!StdDraw.hasNextKeyTyped()) {
+                continue;
+            }
+            Character inputChar = Character.valueOf(Character.toLowerCase(StdDraw.nextKeyTyped()));
+            if (inputChar.equals('s')) {
+                if (Seed.isEmpty()) {
+                    continue;
+                } else {
+                    try {
+                        long returnlong = Long.valueOf(Seed);
+                        return returnlong;
+                    } catch (NumberFormatException e) {
+                        throw e;
+                    }
+                }
+            }
+            if (Character.isDigit(inputChar)) {
+                Seed = Seed + inputChar;
+                Font seedFont = new Font("Verdana", Font.PLAIN, 12);
+                StdDraw.setFont(seedFont);
+                StdDraw.clear(Color.BLACK);
+                StdDraw.text(canvasWIDTH / 2, canvasHEIGHT * 2 / 3, Seed);
+                StdDraw.setFont(font);
+                StdDraw.text(canvasWIDTH / 2, canvasHEIGHT / 2, "Please input the seed and end with 'S'!");
+                StdDraw.show();
+            } else {
+                Seed = "";
+                Font seedFont = new Font("Verdana", Font.PLAIN, 12);
+                StdDraw.setFont(seedFont);
+                StdDraw.clear(Color.BLACK);
+                StdDraw.text(canvasWIDTH / 2, canvasHEIGHT * 2 / 3, Seed);
+                StdDraw.setFont(font);
+                StdDraw.text(canvasWIDTH / 2, canvasHEIGHT / 2, "Please input the seed and end with 'S'!");
+                StdDraw.show();
+            }
+        }
+    }
+
+    private TETile[][] generateWorld(long Seed) {
+        TETile[][] world = new TETile[WIDTH][HEIGHT];
+        ArrayList<Position> alternatePosition = new ArrayList<>();
+        ArrayList<Position> vertex;
+        ArrayList<Position> wallPosition;
+        Random RANDOM = new Random(Seed);
+        int trynum = 10;
+        vertex = Hallway.initializeWorld(world);
+        Hallway.generateMaze(world, alternatePosition, RANDOM);
+        Hallway.delHallway(world, vertex, RANDOM);
+        for (int n = 0; n < trynum; n += 1) {
+            Room.generateRoom(world, RANDOM, vertex);
+        }
+        wallPosition = Wall.roomWall(world);
+        Wall.GenerateLockedDoor(world, wallPosition, RANDOM);
+        Player.generatePlayer(vertex, world, RANDOM);
+        return world;
+    }
+
+    private TETile[][] loadWorld() {
+        File f = new File("./saveWorld.ser");
+        if (f.exists()) {
+            try {
+                FileInputStream fs = new FileInputStream(f);
+                ObjectInputStream os = new ObjectInputStream(fs);
+                TETile[][] loadWorld = (TETile[][]) os.readObject();
+                Player.xPos = (int) os.readObject();
+                Player.yPos = (int) os.readObject();
+                os.close();
+                return loadWorld;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return new TETile[WIDTH][HEIGHT];
+    }
+
+    private void saveWorld(TETile[][] world) {
+        File f = new File("./saveWorld.ser");
+        try {
+            if (!f.exists()) {
+                f.createNewFile();
+            }
+            FileOutputStream fs = new FileOutputStream(f);
+            ObjectOutputStream os = new ObjectOutputStream(fs);
+            os.writeObject(world);
+            os.writeObject(Player.xPos);
+            os.writeObject(Player.yPos);
+            os.close();
+        }  catch (FileNotFoundException e) {
+            System.out.println("file not found");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+
+    private long getInputSeed(String input) {
+        int indexS = input.indexOf('s');
+
+        String seed = input.substring(1, indexS);
+
+        return Long.parseLong(seed);
+    }
+
+    private void generateInitWorld(TETile[][] world) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        ter.renderFrame(world);
+    }
+
+    private void drawFrame(TETile[][] world) {
+        StdDraw.clear(Color.BLACK);
+
+
+        Font mazeFont = new Font("Monaco", Font.BOLD, 14);
+        StdDraw.setFont(mazeFont);
+        int numXTiles = world.length;
+        int numYTiles = world[0].length;
+        int xOffset = 0;
+        int yOffset = 0;
+        for (int x = 0; x < numXTiles; x += 1) {
+            for (int y = 0; y < numYTiles; y += 1) {
+                if (world[x][y] == null) {
+                    throw new IllegalArgumentException("Tile at position x=" + x + ", y=" + y
+                            + " is null.");
+                }
+                world[x][y].draw(x + xOffset, y + yOffset);
+            }
+        }
+
+        Font font = new Font("Verdana", Font.BOLD, 16);
+        StdDraw.setFont(font);
+        StdDraw.setPenColor(Color.WHITE);
+        String s = mouseTile(world);
+        StdDraw.text(3, world[0].length - 2, s);
+
+
+        StdDraw.show();
+    }
+
+    private String mouseTile(TETile[][] world) {
+        int mouseX = (int) StdDraw.mouseX();
+        int mouseY = (int) StdDraw.mouseY();
+        String s = null;
+        if (mouseX >= world.length || mouseX < 0 || mouseY >=world[0].length || mouseY < 0) {
+            return s = "";
+        }
+        TETile t = world[mouseX][mouseY];
+        if (ReturnTile.returnFloor.equals(t)) {
+            s = "floor";
+        } else if (ReturnTile.returnNothing.equals(t)) {
+            s = "Nothing";
+        } else if (ReturnTile.returnPlayer.equals(t)) {
+            s = "player";
+        } else if (ReturnTile.returnWall.equals(t)) {
+            s = "wall";
+        } else if (ReturnTile.returnLockedDoor.equals(t)) {
+            s = "lockedDoor";
+        } else {
+            throw new IllegalStateException("Unexpected value: " + t);
+        }
+        return s;
+    }
+
+    private void startGame(TETile[][] world, String s) {
+        int mouseX = (int) StdDraw.mouseX();
+        int mouseY = (int) StdDraw.mouseY();
+
+        if (s != null) {
+            char[] sinput = s.toCharArray();
+            for (int n = 0; n < sinput.length; n += 1) {
+                char in = sinput[n];
+                in = Character.toLowerCase(in);
+                if (in == 'w' || in == 's' || in == 'a' || in == 'd') {
+                    Player.move(in, world);
+                    //gameOver = Check.gameOver(player, world);
+                } else {
+                    if (in == ':' && n != sinput.length - 1) {
+                        if (sinput[n + 1] == 'q') {
+                            saveWorld(world);
+                            System.exit(0);
+                        }
+                    }
+                }
+            }
+        }
+
+        while (!gameOver) {
+            if (mouseX != (int) StdDraw.mouseX() || mouseY != (int) StdDraw.mouseY()) {
+                mouseX = (int) StdDraw.mouseX();
+                mouseY = (int) StdDraw.mouseY();
+                drawFrame(world);
+            }
+
+           if (!StdDraw.hasNextKeyTyped()) {
+                continue;
+            }
+            char in = Character.toLowerCase(StdDraw.nextKeyTyped());
+            if (in == 'w' || in == 's' || in == 'a' || in == 'd') {
+                Player.move(in, world);
+                //gameOver = Check.gameOver(player, world);
+                drawFrame(world);
+            } else {
+                if (in == ':') {
+                    char inin;
+                    while (true) {
+                        if (!StdDraw.hasNextKeyTyped()) {
+                            continue;
+                        }
+                        inin = Character.toLowerCase(StdDraw.nextKeyTyped());
+                        break;
+                    }
+                    if (inin == 'q') {
+                        saveWorld(world);
+                        System.exit(0);
+                    }
+                } else {
+                    continue;
+                }
+            }
+        }
+
+        drawGameOver();
+        StdDraw.pause(1500);
+        System.exit(0);
+    }
+
+    private void drawGameOver() {
+        InitializeCanvas(canvasWIDTH, canvasHEIGHT);
+
+        Font bigFont = new Font("Verdana", Font.BOLD, 22);
+        StdDraw.setFont(bigFont);
+        StdDraw.text(canvasWIDTH / 2, canvasHEIGHT / 2, "Congralation! You win!");
+
+        StdDraw.show();
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.playWithKeyboard();
     }
 }
--- proj2/byog/lab6/MemoryGame.java
+++ proj2/byog/lab6/MemoryGame.java
@@ -1,9 +1,12 @@
 package byog.lab6;
 
+import byog.TileEngine.TETile;
+import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.introcs.StdDraw;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.security.DrbgParameters;
 import java.util.Random;
 
 public class MemoryGame {
@@ -19,17 +22,19 @@ public class MemoryGame {
                                                    "Too easy for you!", "Wow, so impressive!"};
 
     public static void main(String[] args) {
+/*
         if (args.length < 1) {
             System.out.println("Please enter a seed");
             return;
         }
+*/
 
-        int seed = Integer.parseInt(args[0]);
-        MemoryGame game = new MemoryGame(40, 40);
+        long seed = Long.parseLong("1000");
+        MemoryGame game = new MemoryGame(40, 40, seed);
         game.startGame();
     }
 
-    public MemoryGame(int width, int height) {
+    public MemoryGame(int width, int height, long seed) {
         /* Sets up StdDraw so that it has a width by height grid of 16 by 16 squares as its canvas
          * Also sets up the scale so the top left is (0,0) and the bottom right is (width, height)
          */
@@ -44,31 +49,100 @@ public class MemoryGame {
         StdDraw.enableDoubleBuffering();
 
         //TODO: Initialize random number generator
+        rand = new Random(seed);
     }
 
     public String generateRandomString(int n) {
         //TODO: Generate random string of letters of length n
-        return null;
+        String randomString = "";
+        for (int i = 0; i < n; i += 1) {
+            char randomChar = CHARACTERS[rand.nextInt(CHARACTERS.length)];
+            randomString += randomChar;
+        }
+        return randomString;
     }
 
     public void drawFrame(String s) {
         //TODO: Take the string and display it in the center of the screen
         //TODO: If game is not over, display relevant game information at the top of the screen
+
+        // Take the string and display it in the center of the screen
+        StdDraw.clear(Color.BLACK);
+        Font font = new Font("Verdana", Font.BOLD, 30   );
+        StdDraw.setFont(font);
+        StdDraw.setPenColor(Color.WHITE);
+        StdDraw.text(width / 2, height / 2, s);
+
+
+        // If game is not over, display relevant game information at the top of the screen
+        if (!gameOver) {
+            Font UIfont = new Font("Verdana", Font.BOLD, 20);
+            StdDraw.setFont(UIfont);
+            StdDraw.textLeft(1, height - 1, "Round: " + round);
+            StdDraw.textRight(width - 1, height - 1, ENCOURAGEMENT[rand.nextInt(ENCOURAGEMENT.length)]);
+            StdDraw.text(width / 2, height - 1, String.valueOf(StdDraw.mouseX()));
+            StdDraw.line(0,height - 2, width, height - 2);
+        }
+
+        StdDraw.show();
     }
 
     public void flashSequence(String letters) {
         //TODO: Display each character in letters, making sure to blank the screen between letters
+        for (int i = 0; i < letters.length(); i++) {
+            drawFrame(letters.substring(i, i+1));
+            StdDraw.pause(1000);
+            drawFrame("");
+            StdDraw.pause(500);
+        }
     }
 
     public String solicitNCharsInput(int n) {
         //TODO: Read n letters of player input
-        return null;
+        String input = "";
+        drawFrame(input);
+
+        while (input.length() < n) {
+            if (!StdDraw.hasNextKeyTyped()) {
+                continue;
+            }
+            char in = StdDraw.nextKeyTyped();
+            if (in == '\b' && input.length() > 0) {
+                input = input.substring(0,input.length() - 1);
+            } else {
+                input += String.valueOf(in);
+            }
+            drawFrame(input);
+
+        }
+        StdDraw.pause(1000);
+        return input;
     }
 
     public void startGame() {
         //TODO: Set any relevant variables before the game starts
-
+        round = 1;
+        gameOver = false;
+        playerTurn = false;
         //TODO: Establish Game loop
+        while (!gameOver) {
+            drawFrame("Round:" + round);
+            StdDraw.pause(1500);
+
+            String randomString = generateRandomString(round);
+            flashSequence(randomString);
+            playerTurn = true;
+            String input = solicitNCharsInput(round);
+
+            playerTurn = false;
+
+            if (input.equals(randomString)) {
+                round += 1;
+            } else {
+                drawFrame("Game Over! You made it to round:" + round);
+                gameOver = true;
+            }
+        }
     }
 
 }
